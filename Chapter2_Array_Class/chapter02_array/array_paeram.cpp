f5f197d8125b92c57c2b5ec34ff8be154c644804
taepyeong0872 <qkrxovud123@naver.com>
2023-03-29 11:48:11 +0900

    arry

---
+++ Chapter2_Array_Class/chapter02_array/array_paeram.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#define MAX_SIZE 10
+
+void sub(int x, int arr[])
+{
+	x = 10;
+	arr[0] = 10;
+}
+
+int main()
+{
+	int var;
+	int list[MAX_SIZE];
+
+	var = 0
+		list[0] = 0;
+	sub(var, list);
+	printf("var=%d, list[0]=%dwn", var, list[0])
+
+		return 0;
+}
